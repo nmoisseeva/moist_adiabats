db65d22016b2f81a9b3dee83dd4bddb198e67ec1
Nadya Moisseeva <nmoisseeva@eos.ubc.ca>
2017-05-30 20:29:01 -0700

    new working code

--- THfcnTP.py
+++ THfcnTP.py
@@ -3,15 +3,15 @@
 # Created by: nmoissee@eoas.ubc.ca May 2017
 #=======================================================
 #INPUT
-Tmin =-60.
-Tmax =40.
-Ptop = 1    #kPa - upper atmosphere limit surface
+Tmin =-80
+Tmax =40
+THmin =-80    
+THmax = 90
+
+Ptop = 1    #kPa - upper atmosphere limit surface (from Part 1)
 degree =10      #degree of polinomial to model the curves
-
-THmin =-60    
-THmax = 60
-
 #=======================================================
+
 #supress warnings
 import warnings
 def fxn():
@@ -26,32 +26,21 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit, minimize
 from scipy import spatial
 
-# #thermodynamic constants
-# Rd = 287.058    #[J K^-1 kg^-1] gas constant for dry air
-# Rv = 461.5  #[J K^-1 kg^-1] gas constant for water vapour
-# Cp = 1006.  #[J K^-1 kg^-1] specific heat of dry air at constant pressure
+#constants
 T0 = 273.16     #standard temperature
 P0 = 101.3      #kPa
-# e0 = 0.611657   #kPa: adjusted Clausius-Clayperon constant (Koutsoyiannis 2011)
 
-# #derived constants
-# Eps = Rd/Rv     #dimensionless
-# c1 = Rd/Cp      #dimensionless
-
-#create temp and pressure axes
+#create temp and pressure axes (from Part 1) - DO NOT CHANGE UNLESS PART 1 IS RECALCULATED
 PrangeList = np.concatenate((np.arange(P0,10,-0.0001),np.arange(10,2,-0.00001),np.arange(2,Ptop,-0.000001)))
 Prange = PrangeList[:-1]
 
-#monotonically select points every 0.1 kPa for fitting
-segments = [95,11,10]
+#create a custom pressure range to aid fitting
+segments = [10,2,Ptop]
 segIdx = [np.argmin(abs(PrangeList - i)) for i in segments]
-PrangeIdx = np.concatenate((np.arange(P0,segIdx[0],1,dtype=int),np.arange(segIdx[0],segIdx[1],1000,dtype=int),np.arange(segIdx[1],segIdx[2],100,dtype=int)))
+# PrangeIdx = np.concatenate((np.arange(P0,segIdx[0],1,dtype=int),np.arange(segIdx[0],segIdx[1],1000,dtype=int),np.arange(segIdx[1],segIdx[2],100,dtype=int)))
+
+PrangeIdx = np.concatenate((np.arange(P0,segIdx[0],1000,dtype=int),np.arange(segIdx[0],segIdx[1],10000,dtype=int)))
 PrangeFit = Prange[PrangeIdx]
-
-
-# Psubrange = np.concatenate((np.arange(P0,90,-0.0001),np.arange(90,6,-0.0001),np.arange(6,5,-0.0001)))
-# PrangeIdx = [np.argmin(abs(PrangeList - i)) for i in Psubrange]
-# PrangeFit = Prange[PrangeIdx]
 
 # PrangeIdx = np.arange(0,1413000,10)
 # PrangeFit = Prange[PrangeIdx]
@@ -63,10 +52,8 @@ nThetaW = np.arange(THmin,THmax)
 #set up temperature ranges
 nTw = np.arange(Tmin,Tmax,0.5)
 nTwk = [i + T0 for i in nTw]
-# nTwlist = [round(i,2) for i in nTw]   
 
 cs = plt.contour(nThetaW,PrangeFit,arrayTHw[:,PrangeIdx].T,nTwk)
-# cs = plt.contour(nThetaW,Prange,arrayTHw[:,:].T,nTwk[:2])
 
 plt.gca().invert_yaxis()
 plt.show()
@@ -79,9 +66,7 @@ for nT, T in enumerate(nTwk):
         path_vals =cs_paths[0]
         vtx = path_vals.vertices
         tnorm = (vtx[:,0] - nTw[nT])
-        # print tnorm[-1]
         arrayTw.append(tnorm)
-        # arrayTw.append(vtx[:,0])
         arrayP.append(vtx[:,1])
 
 
@@ -95,18 +80,8 @@ print('MAE for polynomial fit of Trefcurve: %.2E' %MAE_Tref)
 
 # np.savetxt('THrefcoeffs.txt',THref_fit.coeffs)
 
-# arrayTnorm = []
-# for nT, T in enumerate(nTwk[-3:]):
-#     arrayTnorm.append(Tref_fit(arrayP[nT])/arrayTw[nT])
-#     # plt.plot(arrayTnorm[nT],arrayP[nT],'g')
-#     plt.plot(Tref_fit(arrayP[nT]),'r')
-#     plt.plot(arrayTw[nT],'b')
-# plt.gca().invert_yaxis()
-# # plt.xlim([0,5])
-# plt.show()
-
-#plot fit of single adiabat THref
-# plt.title('$\\theta_{ref} = \\theta_{40C}$ POLYNOMIAL FIT')
+#plot fit of single adiabat Tref
+# plt.title('$\\T_{ref} = \\theta_{%s}$ POLYNOMIAL FIT' %nTwk[refIdx])
 plt.plot(Tref,Pref,'g')
 plt.plot(Tref_fit(Pref),Pref,'r')
 plt.gca().invert_yaxis()
@@ -114,7 +89,7 @@ plt.gca().invert_yaxis()
 # plt.ylim([6,5])
 plt.xlabel('normalized temperature')
 plt.ylabel('pressure [kPa]')
-# plt.savefig('./figs/THref.pdf')
+# plt.savefig('./figs/Tref.pdf')
 plt.show()
 plt.close()
 
@@ -125,10 +100,8 @@ numterms = degree+1
 store_args = np.zeros((numterms,len(nTwk)))
 tags = ['k%s' %i for i in range(numterms)]
 for nT in range(len(nTwk)):
-    # main_pfit = np.poly1d(np.polyfit(arrayP[nT],arrayTw[nT],degree))
     main_pfit = np.poly1d(np.polyfit(Tref_fit(arrayP[nT]),arrayTw[nT],degree))
     store_args[:,nT] = main_pfit.coeffs
-    # plt.plot(main_pfit(arrayP[nT]),arrayP[nT],'r')
     plt.plot(main_pfit(Tref_fit(arrayP[nT])),arrayP[nT],'r')
     plt.plot(arrayTw[nT],arrayP[nT],'b')
 plt.gca().invert_yaxis()
@@ -158,7 +131,7 @@ for iDeg in range(degree):
     plt.plot(nTwk,store_args[iDeg,:],'g')
     plt.plot(nTwk,fitFCNs[iDeg](nTwk),'r')
 plt.subplots_adjust(top = .92, hspace=0.4, wspace=0.4, left=0.05, right=0.97, bottom=0.05)
-# plt.savefig('./figs/fit_params.pdf')
+plt.savefig('./figs/fit_params_T.pdf')
 plt.show()
 plt.close()
 
@@ -185,11 +158,6 @@ arrayDiff = []
 for nT, T in enumerate(nTwk):
     diff = arrayTw[nT][::10] - arrayTHfit[nT]
     arrayDiff.append(diff)
-    MAE = np.mean(abs(diff),0)
-    print MAE
-
-
-for nT in range(len(nTwk)):
     plt.plot(arrayTw[nT],arrayP[nT],'b')
     plt.plot(arrayTHfit[nT],arrayP[nT][::10],'r' )
 plt.gca().invert_yaxis()
@@ -197,116 +165,23 @@ plt.show()
 plt.close()
 
 
+maxLen = len(sorted(arrayDiff,key=len, reverse=True)[0])
+error2D = np.zeros((len(arrayDiff),maxLen))*np.nan
+for nT, T in enumerate(nTwk):
+    vals = arrayDiff[nT]
+    error2D[nT,:len(vals)] = vals
+
+
 plt.figure(figsize=(8,6))
 plt.title('ABSOLUTE ERROR (C)')
-for nT,T in enumerate(nTwk):
-    plt.scatter(arrayTw[nT][::10]+nTw[nT],arrayP[nT][::10],s=1,c=arrayDiff[nT],vmin=-0.1,vmax=0.1, cmap='RdBu_r')
-plt.gca().invert_yaxis()
-plt.xlim([-40,40])
-# plt.colorbar()
-# plt.savefig('./figs/DiffContoursTemp.pdf')
-# plt.show()
+# for nT,T in enumerate(nTwk):
+#     plt.scatter(arrayTw[nT][::10]+nTw[nT],arrayP[nT][::10],s=1,c=arrayDiff[nT],vmin=-0.05,vmax=0.05, cmap='RdBu_r')
+plt.contourf(error2D.T,cmap='RdBu_r',vmin=-0.05, vmax=0.05)
 
-
-plt.show()
-plt.close()
-
-
-# plt.figure(figsize=(8,6))
-# plt.title('ABSOLUTE ERROR (C)')
-# plt.imshow(arrayDiffTemp.T,aspect='auto',origin='lower',cmap='RdBu_r',vmin=-0.05, vmax=0.05)
-# plt.xlabel("temperature [C]")
-# plt.ylabel("pressure [kPa]")
-# ax = plt.gca()
-# ax.set_xticks(np.arange(0,len(nThetaW),10))
-# ax.set_yticks([1300,51300,100300])
-# ax.set_xticklabels(nThetaW[::10])
-# ax.set_yticklabels([100,50,1])
-# plt.colorbar()
-# plt.savefig('./figs/DiffContoursTemp.pdf')
-# plt.show()
-
-# #=====================PLOTTING===========================
-
-
-# #plot true and fitted normalized saturated adiabats
-# plt.figure(figsize=(8,6))
-# plt.title('TRUE AND MODELLED $\\theta_{norm}$')
-# plt.plot(Prange,arrayTHnorm[1::10,:].T,color='g', label='directly computed $\\theta_{norm}$')
-# plt.plot(Prange,arrayTHfit[1::10,:].T,'r',label='modelled $\\theta_{norm}$')
-# plt.gca().invert_xaxis()
-# plt.xlim([101,1])
-# plt.ylim([0,1.1])
-# plt.grid()
-# plt.ylabel("normalized moist adiabats")
-# plt.xlabel("pressure [kPa]")
-# plt.savefig('./figs/THfit.pdf')
-# plt.show()
-
-# #plot error distribution contours
-# plt.figure(figsize=(8,6))
-# plt.title('ERROR CONTOURS OF $\\theta_{norm}$')
-# plt.imshow(arrayDiff.T,aspect='auto',origin='lower',cmap='RdBu_r',vmin=-0.005,vmax=0.005)
-# plt.xlabel("temperature [C]")
-# plt.ylabel("pressure [kPa]")
-# ax = plt.gca()
-# ax.set_xticks(np.arange(0,len(nThetaW),10))
-# ax.set_yticks([1300,51300,100300])
-# ax.set_xticklabels(nThetaW[::10])
-# ax.set_yticklabels([100,50,1])
-# plt.colorbar()
-# plt.savefig('./figs/DiffContours.pdf')
-# plt.show()
-
-# #plot error distribution contours in degrees
-# plt.figure(figsize=(8,6))
-# plt.title('ABSOLUTE ERROR (C)')
-# plt.imshow(arrayDiffTemp.T,aspect='auto',origin='lower',cmap='RdBu_r',vmin=-0.05, vmax=0.05)
-# plt.xlabel("temperature [C]")
-# plt.ylabel("pressure [kPa]")
-# ax = plt.gca()
-# ax.set_xticks(np.arange(0,len(nThetaW),10))
-# ax.set_yticks([1300,51300,100300])
-# ax.set_xticklabels(nThetaW[::10])
-# ax.set_yticklabels([100,50,1])
-# plt.colorbar()
-# plt.savefig('./figs/DiffContoursTemp.pdf')
-# plt.show()
-
-# #plot stuve's diagram with modelled adiabats
-# plt.figure(figsize=(9,6))
-# plt.title('TESTING')
-# plt.plot(arrayTHw[-1,:]-T0,Prange, 'b',label='moist adiabat $\\theta_w$')
-# plt.plot(arrayTHd[-1,:]-T0,Prange, 'r--',label='dry adiabat $\\theta_d$')
-# plt.plot(arrayTHe[-1,:]-T0,Prange, 'g:',label='equivalent potential temperature $\\theta_e}$')
-# plt.plot(arrayTHwm[-1,:]-T0,Prange, 'y',label='modelled moist adiabat $\\theta_{mod}$')
-# plt.plot(arrayTHw[0::5,:].T-T0,Prange, 'b')
-# plt.plot(arrayTHd[0::10,:].T-T0,Prange, 'r--')
-# plt.plot(arrayTHe[0::10,:].T-T0,Prange, 'g:')
-# plt.plot(arrayTHwm[0::5,:].T-T0,Prange, 'y')
-# plt.ylim([40,101])
+# plt.imshow(error2D,aspect='auto',origin='lower',cmap='RdBu_r',vmin=-0.05, vmax=0.05)
 # plt.gca().invert_yaxis()
 # plt.xlim([-40,40])
-# plt.grid()
-# plt.xlabel("temperature [C]")
-# plt.ylabel("pressure [kPa]")
-# plt.legend(loc='upper right',fontsize=12)
-# # plt.savefig('./figs/stuve.pdf')
-# plt.show()
-# plt.close()
-
-
-# # #plot THw(TP)
-# # plt.figure(figsize=(8,6))
-# # plt.title('$\\theta_{w}(T,P)$')
-# # plt.imshow(WofTP_MAE.T,aspect='auto',origin='lower',cmap='RdBu_r')
-# # plt.xlabel("temperature [C]")
-# # plt.ylabel("pressure [kPa]")
-# # ax = plt.gca()
-# # ax.set_xticks(np.arange(0,len(nThetaW),5))
-# # # ax.set_yticks([1300,51300,100300])
-# # ax.set_xticklabels(nThetaW[::5])
-# # # ax.set_yticklabels([100,50,1])
-# # plt.colorbar()
-# # # plt.savefig('./figs/DiffContoursTemp.pdf')
-# # plt.show()
+plt.colorbar()
+plt.savefig('./figs/DiffContoursTHw.pdf')
+plt.show()
+plt.close()
